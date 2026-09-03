9ca601d719aedd70456aba2a31faa8a1357e39b7
Priyanshuf1 <apriyanshu540@gmail.com>
2026-09-03 22:13:15 +0530

    feat: add Google Tag Manager container GTM-MT99KWV4 in head and noscript in body

--- patch.js
+++ patch.js
@@ -561,6 +561,26 @@ if (!html.includes('ycm4i77tiz')) {
 `);
 }
 
+
+// Inject Google Tag Manager if missing
+if (!html.includes('GTM-MT99KWV4')) {
+    html = html.replace('<head>', '<head>\n' + `
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MT99KWV4');</script>
+<!-- End Google Tag Manager -->
+`);
+    html = html.replace(/<body[^>]*>/i, (match) => match + '\n' + `
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MT99KWV4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+`);
+}
+
 // Inject Firebase init before other custom scripts
 if (!html.includes('firebase-init.js')) {
     const firebaseScripts = `
